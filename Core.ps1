8db57f6c73f04d96e0882f9b88623dead7c32605
sarukiti_satan <sarukiti1891@gmail.com>
2020-03-07 13:05:36 +0900

    GUI表示に関する情報をxamlファイルに分けた

--- Core.ps1
+++ Core.ps1
@@ -1,59 +1,19 @@
 $ErrorActionPreference = "stop"
 Set-PSDebug -Strict
 Add-Type -AssemblyName PresentationFramework
-
-[xml]$xaml = @'
-<Window
-    xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
-    xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
-    Title="WindowsStartdash"
-    Width="500" Height="200pt">
-    <StackPanel>
-        <StackPanel Orientation="Horizontal" HorizontalAlignment="Left">
-            <StackPanel x:Name="application">
-                <RadioButton Content="PlaceHolder"></RadioButton>
-                <RadioButton Content="PlaceHolder"></RadioButton>
-                <RadioButton Content="PlaceHolder"></RadioButton>
-                <RadioButton Content="PlaceHolder"></RadioButton>
-                <RadioButton Content="PlaceHolder"></RadioButton>
-                <RadioButton Content="PlaceHolder"></RadioButton>
-                <RadioButton Content="PlaceHolder"></RadioButton>
-                <RadioButton Content="PlaceHolder"></RadioButton>
-                <RadioButton Content="PlaceHolder"></RadioButton>
-                <RadioButton Content="PlaceHolder"></RadioButton>
-                <RadioButton Content="PlaceHolder"></RadioButton>
-            </StackPanel>
-            <StackPanel x:Name="Overview">
-                <Label x:Name="label01" Content="  " HorizontalAlignment="Left" Margin="0,-5,0,0" VerticalAlignment="Top" FontSize="10"/>
-                <Label x:Name="label02" Content="  " HorizontalAlignment="Left" Margin="0,-8,0,0" VerticalAlignment="Top" FontSize="10"/>
-                <Label x:Name="label03" Content="  " HorizontalAlignment="Left" Margin="0,-8,0,0" VerticalAlignment="Top" FontSize="10"/>
-                <Label x:Name="label04" Content="  " HorizontalAlignment="Left" Margin="0,-8,0,0" VerticalAlignment="Top" FontSize="10"/>
-                <Label x:Name="label05" Content="  " HorizontalAlignment="Left" Margin="0,-8,0,0" VerticalAlignment="Top" FontSize="10"/>
-                <Label x:Name="label06" Content="  " HorizontalAlignment="Left" Margin="0,-8,0,0" VerticalAlignment="Top" FontSize="10"/>
-                <Label x:Name="label07" Content="  " HorizontalAlignment="Left" Margin="0,-8,0,0" VerticalAlignment="Top" FontSize="10"/>
-                <Label x:Name="label08" Content="  " HorizontalAlignment="Left" Margin="0,-8,0,0" VerticalAlignment="Top" FontSize="10"/>
-                <Label x:Name="label09" Content="  " HorizontalAlignment="Left" Margin="0,-8,0,0" VerticalAlignment="Top" FontSize="10"/>
-                <Label x:Name="label10" Content="  " HorizontalAlignment="Left" Margin="0,-8,0,0" VerticalAlignment="Top" FontSize="10"/>
-                <Label x:Name="label11" Content="  " HorizontalAlignment="Left" Margin="0,-8,0,0" VerticalAlignment="Top" FontSize="10"/>
-            </StackPanel>
-        </StackPanel>
-        <Button x:Name="btn1" Content="Download"  FontSize="10pt" />
-    </StackPanel>
-</Window>
-'@
-
+[xml]$xaml = [XML](Get-Content ./GUI.xaml)
+$XML = [XML](Get-Content ./Application.xml)
 $reader = New-Object System.Xml.XmlNodeReader $xaml
 $frm = [System.Windows.Markup.XamlReader]::Load($reader)
 $application = $frm.FindName("application")
 $msg = $frm.FindName("Overview")
-$XML = [XML](Get-Content ./Application.xml)
 
-# 全てのRadioButtonを非表示にする
+# 蜈ｨ縺ｦ縺ｮRadioButton繧帝撼陦ｨ遉ｺ縺ｫ縺吶ｋ
 foreach ($s in $application.Children) {
     $s.Visibility = "Collapsed"
 }
 
-# RadioButtonのContentを設定し、表示する
+# RadioButton縺ｮContent繧定ｨｭ螳壹＠縲∬｡ｨ遉ｺ縺吶ｋ
 $applicationList = $XML.Application.Software.Name
 $n = 0
 $children = $application.Children
@@ -64,7 +24,7 @@ foreach ($c in $applicationList) {
     $n += 1
 }
 
-# RadioButtonのContentを設定し、表示する
+# RadioButton縺ｮContent繧定ｨｭ螳壹＠縲∬｡ｨ遉ｺ縺吶ｋ
 $overviewList = $XML.Application.Software.Overview
 $n = 0
 $childrenO = $msg.Children
@@ -75,7 +35,7 @@ foreach ($c in $overviewList) {
     $n += 1
 }
 
-# 選択された項目を返す
+# 驕ｸ謚槭＆繧後◆鬆�逶ｮ繧定ｿ斐☆
 function getChecked($parent) {
     $checked = ""
     foreach ($child in $parent.Children) {
